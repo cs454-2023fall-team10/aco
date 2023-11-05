2c3c0d19f5fac56de4ec3be5f84ae4047beb7ca0
sillysillyman <sillysillyman@sillysillyman.local>
2023-11-05 22:10:23 +0900

    added messed chatbot graph

--- chatbot_graph.py
+++ chatbot_graph.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import json
+import random
+
+random.seed(42)
 
 DG = nx.DiGraph()
+messed_DG = nx.DiGraph()
 
 file_path = "../chatbot-dataset/examples/general-homepage.json"
 
 with open(file_path, 'r') as file:
     data = json.load(file)["sections"]
-    print(type(data))
-    print(data)
 
 # for section in data:
 #     print(type(section))
@@ -26,5 +28,25 @@ for section in data:
     for button in section['buttons']:
         DG.add_edge(section['id'], button['nextSectionId'])
 
-nx.draw(DG, with_labels=True)
+size = len(DG.nodes)
+print(type(DG.nodes))
+for node in DG.nodes:
+    num_of_indices = random.randrange(size)
+    messed_DG.add_node(node)
+    messed_DG.nodes[node]['text'] = DG.nodes[node]['text']
+    for neighbor in random.sample(list(DG.nodes), num_of_indices):
+        messed_DG.add_edge(node, neighbor)
+        if not messed_DG.nodes[node]['text']:
+            messed_DG.nodes[neighbor]['text'] = DG.nodes[neighbor]['text']
+
+plt.subplot(1, 2, 1)
+pos1 = nx.spring_layout(DG)
+nx.draw(DG, with_labels=True, pos=pos1)
+plt.title("Original chatbot graph")
+
+plt.subplot(1, 2, 2)
+pos2 = nx.spring_layout(messed_DG)
+nx.draw(messed_DG, with_labels=True, pos=pos2)
+plt.title("Intentioanlly messed chatbot graph")
+
 plt.show()
