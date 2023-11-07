8bd09bef265b1b8887bba5fcc3adbdeda7bc0663
ddungiii <ggcc503@gmail.com>
2023-11-07 16:53:25 +0900

    chore: divide functions in chatbot_graph

--- chatbot_graph.py
+++ chatbot_graph.py
@@ -5,31 +5,36 @@ import json
 import random
 
 
-random.seed(42)
-
-file_path = "../chatbot-dataset/examples/general-homepage.json"
-with open(file_path, 'r') as file:
-    data = json.load(file)["sections"]
-
 # for section in data:
 #     print(type(section))
 #     print(section)
 #     print()
 
-def make_chatbot_graphs(data):
+
+def make_chatbot_graph():
+    random.seed(42)
+
+    file_path = "../chatbot-dataset/examples/general-homepage.json"
+    with open(file_path, "r") as file:
+        data = json.load(file)["sections"]
+
     DG = nx.DiGraph()
-    messed_DG = nx.DiGraph()
 
     for section in data:
-        DG.add_node(section['id'])
-        DG.nodes[section['id']]['text'] = f"\"{section['text']}\""
+        DG.add_node(section["id"])
+        DG.nodes[section["id"]]["text"] = f"\"{section['text']}\""
 
     for section in data:
-        if section['type'] == 'stop':
+        if section["type"] == "stop":
             continue
-        for button in section['buttons']:
-            DG.add_edge(section['id'], button['nextSectionId'])
+        for button in section["buttons"]:
+            DG.add_edge(section["id"], button["nextSectionId"])
 
+    return DG
+
+
+def messup_graph(DG):
+    messed_DG = nx.DiGraph()
     # Mess up the original graph randomly.
     # size = len(DG.nodes)
     # for node in DG.nodes:
@@ -43,11 +48,39 @@ def make_chatbot_graphs(data):
 
     # Hard-coded messed chatbot graph to make transforamtion graph conveniently.
     messed_DG = DG.copy()
-    removed_edges = [('Q', 'R'), ('Q', 'V'), ('R', 'S'), ('R', 'T'), ('R', 'U'), ('O', 'P'), ('O', 'A'), ('S', 'V'), ('S', 'R'), ('T', 'V'), ('T', 'R'), ('U', 'V'), ('U', 'R')]
-    added_edges = [('Q', 'J'), ('Q', 'K'), ('J', 'S'), ('J', 'T'), ('K', 'U'), ('I', 'R'), ('I', 'V'), ('M', 'O'), ('N', 'P')]
+    removed_edges = [
+        ("Q", "R"),
+        ("Q", "V"),
+        ("R", "S"),
+        ("R", "T"),
+        ("R", "U"),
+        ("O", "P"),
+        ("O", "A"),
+        ("S", "V"),
+        ("S", "R"),
+        ("T", "V"),
+        ("T", "R"),
+        ("U", "V"),
+        ("U", "R"),
+    ]
+    added_edges = [
+        ("Q", "J"),
+        ("Q", "K"),
+        ("J", "S"),
+        ("J", "T"),
+        ("K", "U"),
+        ("I", "R"),
+        ("I", "V"),
+        ("M", "O"),
+        ("N", "P"),
+    ]
     messed_DG.remove_edges_from(removed_edges)
     messed_DG.add_edges_from(added_edges)
 
+    return messed_DG
+
+
+def print_chatbot_graphs(DG, messed_DG):
     plt.subplot(1, 2, 1)
     pos1 = graphviz_layout(DG, prog="dot")
     nx.draw(DG, with_labels=True, pos=pos1)
@@ -61,4 +94,8 @@ def make_chatbot_graphs(data):
 
     plt.show()
 
-make_chatbot_graphs(data)
+
+if __name__ == "__main__":
+    DG = make_chatbot_graph()
+    messed_DG = messup_graph(DG)
+    print_chatbot_graphs(DG, messed_DG)
